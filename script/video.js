68017547710c4fa8acbee2e3948081bca9ee2e7d
ajimcse <mdajimsorkarcse40@gmail.com>
2025-09-04 08:53:07 +0600

    six commtie

--- script/video.js
+++ script/video.js
@@ -4,7 +4,6 @@ const loadCatrogoy = () => {
     .then(data => loadManuDisplay(data.categories))
     .catch(error => console.log(error))
 }
-
  
 const loadActiveButton = (id) => {
   fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
@@ -26,45 +25,59 @@ const activeRemoveKora = () => {
     btn.classList.remove('active')
   }
 }
- 
- 
-const loadDisplayDstels = async (ditels) =>{
-   const url =`https://openapi.programming-hero.com/api/peddy/pet/${ditels}`
+  
+const loadDisplayModal = async (modal) =>{
+   const url =`https://openapi.programming-hero.com/api/peddy/pet/${modal}`
    const res = await fetch(url);
    const data =await res.json()
-  displayditels(data.petData)
+  displayModal(data.petData)
 
 }
  
-const displayditels = (data) =>{
-  const modalContainer =document.getElementById('modalContainer');
+   const loadDisplayModal2 = async (Detils) =>{
+   const url =`https://openapi.programming-hero.com/api/peddy/pet/${Detils}`
+   const res = await fetch(url);
+   const data =await res.json()
+  displayDetils(data.petData)
+
+}
+const displayDetils= (dataModal) =>{
+   console.log(dataModal);
+   const detlasContainer=document.getElementById('modalConten');
+   detlasContainer.innerHTML = `
+    <img src="${dataModal.image}" alt="" />
+    <p>${dataModal.pet_details}</p>
+   `
+
+  //  document.getElementById('showModal').click()
+  document.getElementById('cusTomModal').showModal()
+}
+ 
+  
+const displayModal = (data) =>{
+  const modalContainer =document.getElementById('likeContainer');
   const modal=document.createElement('div')
    modal.innerHTML=`
-   <div class='w-[200px] mx-auto my-5 pb-10'> <img src="${data.image}" alt="" />
-     <h2 class='font-bold'>${data.category}</h2>
+   <div class='w-[160px] mx-auto my-5 pb-10 '> <img class='rounded-2xl' src="${data.image}" alt="" />
+     <h2 class='font-bold '>${data.category}</h2>
    </div>
    
    ` 
    modalContainer.append(modal)
    
-}
-
+} 
  const loadManuDisplay = (datas) => {
   const manuContiner = document.getElementById('manu')
   datas.forEach((item) => {
     const buttonContainer = document.createElement('div')
-
     buttonContainer.innerHTML = `
-     
       <button id='btn-${item.category}' onclick='loadActiveButton("${item.category}")' class='border rounded-2xl border-cyan-200 px-20 py-4 acBtn'>
       <img   src="${item.category_icon}" alt=" " class="w-10 h-10  ">
           <span class='font-bold'>${item.category}</span>
      </button>
-      
-  
-     `
-
-    manuContiner.append(buttonContainer)
+       
+     ` 
+      manuContiner.append(buttonContainer)
 
   });
 };
@@ -102,15 +115,11 @@ const loadMainCategoroyDisplay = (lodaMCaterogoy) => {
     const divContainer = document.createElement('div');
 
     divContainer.innerHTML = `
-
-  
    <div class="card  bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
     <img src="${item.image}" alt="Shoes" class="rounded-xl h-full w-full" />
-  
-  </figure>
-  
-  <div class="card-body ">
+   </figure>
+   <div class="card-body ">
         <h3 class='font-bold text-2xl'> ${item.pet_name}</h3>
          <div class='flex gap-2'>
           <img class='w-[20px] ' src="https://img.icons8.com/?size=96&id=n9IKS3yfXwF7&format=png" alt="" />
@@ -128,14 +137,12 @@ const loadMainCategoroyDisplay = (lodaMCaterogoy) => {
           <img class='w-[20px] ' src="https://img.icons8.com/?size=100&id=aTrWEDlg4xEW&format=png" alt="" />
          <h3>Price: ${item.price}</h3>
          </div>
-        
-
-    <div class="card-actions justify-between pt-5">
-   <img onclick='loadDisplayDstels(${item.petId})' class=' btn border text-green-400 rounded ' src="https://img.icons8.com/?size=48&id=82788&format=png" alt="" />
-      <button class="btn border text-green-400 rounded"> Adopt 
+       <div class="card-actions justify-between pt-5">
+   <img onclick='loadDisplayModal(${item.petId})' class=' btn border text-green-400 rounded ' src="https://img.icons8.com/?size=48&id=82788&format=png" alt="" />
+      <button class="btn btn-sm text-green-400 rounded"> Adopt 
       </button>
 
-      <button  class="btn btn-sm text-green-400">Detalis</button>
+      <button onclick='loadDisplayModal2(${item.petId})'  class="btn btn-sm text-green-400">Detalis</button>
     </div>
   </div>
 </div>
